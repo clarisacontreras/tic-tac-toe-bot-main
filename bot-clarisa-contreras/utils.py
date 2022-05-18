6cadf088420137d4ca354cac3da8df6182131d8f
Clarisa Contreras <clarisacontreras@ufm.edu>
2022-05-17 19:40:17 -0600

    Update utils.py

--- bot-clarisa-contreras/utils.py
+++ bot-clarisa-contreras/utils.py
@@ -81,37 +81,63 @@ def decide_move(board: list, player_id: str) -> list:
         return [0,2]
     if board [1][0] == player_id and board [1][1] == player_id and board [1][2] == "-":
         return [1,2]
+    if board [1][1] == player_id and board [0][1] == player_id and board [1][2] == "-":
+        return [1,2]
     if board [2][0] == player_id and board [2][1] == player_id and board [2][2] == "-":
+        return [2,2]
+    if board [2][1] == player_id and board [2][0] == player_id and board [2][2] == "-":
         return [2,2]
     if board [0][0] == player_id and board [1][0] == player_id and board [2][0] == "-":
         return [2,0]
-    if board [0][1] == player_id and board [1][1] == player_id and board [21] == "-":
+    if board [1][0] == player_id and board [0][0] == player_id and board [2][0] == "-":
+        return [2,0]
+    if board [0][1] == player_id and board [1][1] == player_id and board [2][1] == "-":
+        return [2,1]
+    if board [1][1] == player_id and board [0][1] == player_id and board [2][1] == "-":
         return [2,1]
     if board [0][2] == player_id and board [1][2] == player_id and board [2][2] == "-":
         return [2,2]
+    if board [1][2] == player_id and board [0][2] == player_id and board [2][2] == "-":
+        return [2,2]
     if board [0][2] == player_id and board [0][1] == player_id and board [0][0] == "-":
+        return [0,0]
+    if board [0][1] == player_id and board [0][2] == player_id and board [0][0] == "-":
         return [0,0]
     if board [1][2] == player_id and board [1][1] == player_id and board [1][0] == "-":
         return [1,0]
+    if board [1][1] == player_id and board [1][2] == player_id and board [1][0] == "-":
+        return [1,0]
     if board [2][2] == player_id and board [2][1] == player_id and board [2][0] == "-":
+        return [2,0]
+    if board [2][1] == player_id and board [2][2] == player_id and board [2][0] == "-":
         return [2,0]
     if board [2][0] == player_id and board [1][0] == player_id and board [0][0] == "-":
         return [0,0]
+    if board [1][0] == player_id and board [2][0] == player_id and board [0][0] == "-":
+        return [0,0]
     if board [2][1] == player_id and board [1][1] == player_id and board [0][1] == "-":
+        return [0,1]
+    if board [1][1] == player_id and board [2][1] == player_id and board [0][1] == "-":
         return [0,1]
     if board [2][2] == player_id and board [1][2] == player_id and board [0][2] == "-":
         return [0,2]
+    if board [1][2] == player_id and board [2][2] == player_id and board [0][2] == "-":
+        return [0,2]
     if board [0][0] == player_id and board [1][1] == player_id and board [2][2] == "-":
+        return [2,2]
+    if board [1][1] == player_id and board [0][0] == player_id and board [2][2] == "-":
         return [2,2]
     if board [2][2] == player_id and board [1][1] == player_id and board [0][0] == "-":
         return [0,0]
+    if board [1][1] == player_id and board [2][2] == player_id and board [0][0] == "-":
+        return [0,0]
     if board [2][0] == player_id and board [1][1] == player_id and board [0][2] == "-":
+        return [0,2]
+    if board [1][1] == player_id and board [2][0] == player_id and board [0][2] == "-":
         return [0,2]
     if board [0][2] == player_id and board [1][1] == player_id and board [2][0] == "-":
         return [2,0]
-    if board [0][2] == player_id and board [1][1] == player_id and board [2][0] == "-":
-        return [2,0]
-    if board [0][2] == player_id and board [1][1] == player_id and board [2][0] == "-":
+    if board [1][1] == player_id and board [0][2] == player_id and board [2][0] == "-":
         return [2,0]
     if board [0][0] == player_id and board [0][2] == player_id and board [0][1] == "-":
         return [0,1]
@@ -138,37 +164,63 @@ def decide_move(board: list, player_id: str) -> list:
         return [0,2]
     if board [1][0] == oponente and board [1][1] == oponente and board [1][2] == "-":
         return [1,2]
+    if board [1][1] == oponente and board [0][1] == oponente and board [1][2] == "-":
+        return [1,2]
     if board [2][0] == oponente and board [2][1] == oponente and board [2][2] == "-":
+        return [2,2]
+    if board [2][1] == oponente and board [2][0] == oponente and board [2][2] == "-":
         return [2,2]
     if board [0][0] == oponente and board [1][0] == oponente and board [2][0] == "-":
         return [2,0]
-    if board [0][1] == oponente and board [1][1] == oponente and board [21] == "-":
+    if board [1][0] == oponente and board [0][0] == oponente and board [2][0] == "-":
+        return [2,0]
+    if board [0][1] == oponente and board [1][1] == oponente and board [2][1] == "-":
+        return [2,1]
+    if board [1][1] == oponente and board [0][1] == oponente and board [2][1] == "-":
         return [2,1]
     if board [0][2] == oponente and board [1][2] == oponente and board [2][2] == "-":
         return [2,2]
+    if board [1][2] == oponente and board [0][2] == oponente and board [2][2] == "-":
+        return [2,2]
     if board [0][2] == oponente and board [0][1] == oponente and board [0][0] == "-":
+        return [0,0]
+    if board [0][1] == oponente and board [0][2] == oponente and board [0][0] == "-":
         return [0,0]
     if board [1][2] == oponente and board [1][1] == oponente and board [1][0] == "-":
         return [1,0]
+    if board [1][1] == oponente and board [1][2] == oponente and board [1][0] == "-":
+        return [1,0]
     if board [2][2] == oponente and board [2][1] == oponente and board [2][0] == "-":
+        return [2,0]
+    if board [2][1] == oponente and board [2][2] == oponente and board [2][0] == "-":
         return [2,0]
     if board [2][0] == oponente and board [1][0] == oponente and board [0][0] == "-":
         return [0,0]
+    if board [1][0] == oponente and board [2][0] == oponente and board [0][0] == "-":
+        return [0,0]
     if board [2][1] == oponente and board [1][1] == oponente and board [0][1] == "-":
+        return [0,1]
+    if board [1][1] == oponente and board [2][1] == oponente and board [0][1] == "-":
         return [0,1]
     if board [2][2] == oponente and board [1][2] == oponente and board [0][2] == "-":
         return [0,2]
+    if board [1][2] == oponente and board [2][2] == oponente and board [0][2] == "-":
+        return [0,2]
     if board [0][0] == oponente and board [1][1] == oponente and board [2][2] == "-":
+        return [2,2]
+    if board [1][1] == oponente and board [0][0] == oponente and board [2][2] == "-":
         return [2,2]
     if board [2][2] == oponente and board [1][1] == oponente and board [0][0] == "-":
         return [0,0]
+    if board [1][1] == oponente and board [2][2] == oponente and board [0][0] == "-":
+        return [0,0]
     if board [2][0] == oponente and board [1][1] == oponente and board [0][2] == "-":
+        return [0,2]
+    if board [1][1] == oponente and board [2][0] == oponente and board [0][2] == "-":
         return [0,2]
     if board [0][2] == oponente and board [1][1] == oponente and board [2][0] == "-":
         return [2,0]
-    if board [0][2] == oponente and board [1][1] == oponente and board [2][0] == "-":
-        return [2,0]
-    if board [0][2] == oponente and board [1][1] == oponente and board [2][0] == "-":
+    if board [1][1] == oponente and board [0][2] == oponente and board [2][0] == "-":
         return [2,0]
     if board [0][0] == oponente and board [0][2] == oponente and board [0][1] == "-":
         return [0,1]
@@ -186,7 +238,6 @@ def decide_move(board: list, player_id: str) -> list:
         return [1,1]
     if board [0][2] == oponente and board [2][0] == oponente and board [1][1] == "-":
         return [1,1]
-
     
     row = randint(0, 2)
     column = randint(0, 2)
